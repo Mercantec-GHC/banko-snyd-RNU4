785376c64cc5459421c9ed6998024c65468ad6c9
RNU4 <malt2592@gmail.com>
2024-02-29 02:39:25 +0100

    basically finished only need to add plates

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -2,12 +2,18 @@
 
 public struct Plade
 {
+    public bool plade_banko = false;
+    public string name = "none";
+    public bool[] bingo_list = new bool[3]{false,false,false};
+
     public int[,] bankoplade;
     public bool[,] bankoplade_bool;
 
-    public Plade(int[,] bankoplade)
+    
+    public Plade(int[,] bankoplade,string name)
     {
         this.bankoplade = bankoplade;
+        this.name = name;
         this.bankoplade_bool = new bool[,]
         {
             {false, false, false, false, false, false, false, false},
@@ -28,6 +34,35 @@ public struct Plade
         }
     }
 
+    public void check_plate()
+    {
+        if (this.plade_banko==true) return; // already banko no need to check again
+
+        bool banko = true;
+        for (int i = 0; i < this.bankoplade.GetLength(0); i++)
+        {
+            if (bingo_list[i] == false){
+                bingo_list[i] = true;
+                for (int j = 0; j < this.bankoplade.GetLength(1); j++)
+                {
+                    if (this.bankoplade_bool[i, j] == false){
+                        bingo_list[i] = false;
+                        banko = false;
+                    }
+                }
+                
+
+                if (bingo_list[i] == true){ // full line
+                    Console.WriteLine("BINGO! på plade "+this.name+" På linje "+(i+1));
+                }
+            }
+        }
+        if (banko == true){
+            Console.WriteLine("Banko! på plade "+this.name);
+            this.plade_banko = true;
+        }
+    }
+
 }
 
 
@@ -35,23 +70,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Plade plade = new Plade(new int[,]
+
+        Plade[] plade_array = new Plade[2];
+
+        plade_array[0] = new Plade(new int[,]
         {
             {1, 20, 33, 43, 53, 69, 71, 87},
             {5, 23, 37, 48, 59, 67, 73, 88},
             {9, 29, 31, 46, 52, 64, 76, 90}
-        });
-        plade.called_number(20);
-        Console.WriteLine("bankoplade array:");
-        for (int i = 0; i < plade.bankoplade.GetLength(0); i++)
+        },"malthe1");
+
+        plade_array[1] = new Plade(new int[,]
         {
-            for (int j = 0; j < plade.bankoplade.GetLength(1); j++)
+            {1, 20, 33, 43, 53, 69, 71, 87},
+            {5, 23, 37, 48, 59, 67, 73, 88},
+            {9, 29, 31, 46, 52, 64, 76, 90}
+        },"malthe2");
+        
+
+        // get a number from the user
+        while(true){
+        Console.WriteLine("Enter number:");
+        string userstring = "";
+        try {userstring = Console.ReadLine();} catch {
+            Console.WriteLine("console error");
+        }
+        if (userstring=="exit") return;
+        if (int.TryParse(userstring, out int number))
+        {
+            // give number and check plate
+        for (int i = 0; i < plade_array.Length; i++){
+            plade_array[i].called_number(number);
+            plade_array[i].check_plate();
+        }
+        }
+        else
+        {
+            Console.WriteLine("Not a number");
+        }
+        }
+        
+/*
+        Console.WriteLine("bankoplade array:");
+        for (int t = 0;  t < plade_array.Length; t++){
+        for (int i = 0; i < plade_array[t].bankoplade.GetLength(0); i++)
+        {
+            for (int j = 0; j < plade_array[t].bankoplade.GetLength(1); j++)
             {
-                Console.Write(plade.bankoplade[i, j] + ":" + plade.bankoplade_bool[i, j] + " ");
+                Console.Write(plade_array[t].bankoplade[i, j] + ":" + plade_array[t].bankoplade_bool[i, j] + " ");
             }
             Console.WriteLine();
         }
+        Console.WriteLine();
 
-        Console.WriteLine("Hello, World!");
+        }*/
     }
 }
